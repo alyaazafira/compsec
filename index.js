343a2d9d0bead152ed0c0128c8e5fe8fc2ca7329
alyaazafira <yazap73@gmail.com>
2023-12-25 17:14:18 +0800

    all appointmnt

--- index.js
+++ index.js
@@ -479,6 +479,53 @@ app.put('/appointments/:name', authenticateToken, async (req, res) => {
     });
 
     // Get all appointments (for security)
+/**
+ * @swagger
+ * /appointments:
+    get:
+      summary: Get appointments
+      description: Endpoint to retrieve appointments based on optional name filter.'
+      parameters:
+        - in: query
+          name: name
+          description: Optional parameter to filter appointments by name (case-insensitive).
+          type: string
+        - in: header
+          name: Authorization
+          description: 'JWT Token for authentication'
+          type: string
+          format: 'Bearer {token}'
+          required: true
+      produces:
+        - application/json
+      responses:
+        '200':
+          description: 'Successful retrieval of appointments'
+          schema:
+            type: array
+            items:
+              type: object
+              properties:
+                // Define the properties of your appointment object here
+                // Example:
+                _id:
+                  type: string
+                  example: '5feceb66ff1bac7d471ebf78'
+                name:
+                  type: string
+                  example: 'John Doe'
+                // Add more properties as needed
+        '403':
+          description: 'Invalid or unauthorized token'
+          schema:
+            type: string
+            example: 'Invalid or unauthorized token'
+        '500':
+          description: 'Internal server error'
+          schema:
+            type: string
+            example: 'Error retrieving appointments'
+ */
     app.get('/appointments', authenticateToken, async (req, res) => {
       const { name } = req.query;
       const { role } = req.user;
